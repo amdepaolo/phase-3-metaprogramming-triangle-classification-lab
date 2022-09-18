4d7c96df336b4f38313a11b8e8cd5c979f021f21
Adam DePaolo <amdepaolo@gmail.com>
2022-09-18 17:42:54 -0400

    commit tree dd9303f39eab898201fbbd29b2b099428c5e6921

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  def initialize (side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3]
+    if less_than_zero || triangle_inequality
+      raise TriangleError
+    end
+  end
+
+  def kind
+    no_unique_sides = @sides.uniq.length
+    if no_unique_sides == 3
+      return :scalene
+    elsif no_unique_sides == 2
+      return :isosceles
+    else
+      return :equilateral
+    end
+  end
+
+  def less_than_zero
+    @sides.any?{|side| side <= 0}
+  end
+
+  def triangle_inequality
+    perimeter = @sides.sum
+    @sides.any?{|side| side >= perimeter - side}
+  end
+
+  class TriangleError < StandardError
+  end
+    
 end
